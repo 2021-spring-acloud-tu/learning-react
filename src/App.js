34bceb0a4b6f04470e7558f4b4d59be02c6cc013
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-09 19:47:30 -0600

    Showing one, of many, ways to style a...
    ...React functional component.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import './App.css';
 import { Greeting } from './Greeting';
 
 const App = () => {
+
+  const styles = {
+    container: {
+      padding: 20
+      , backgroundColor: new Date().getSeconds() % 2 == 0 ? "lightblue" : "pink"
+    }
+    , heading: {
+      textTransform: "uppercase"
+    }
+  };
+
+
   return (
-    <>
-      <h1>
+    <div
+      style={styles.container}
+    >
+      <h1
+        style={styles.heading}
+      >
         Hello React ! ! !
       </h1>
       <Greeting name="Karly" favoriteNumber={9} favoriteColors={["Red", "Purple", "Green", "Red"]} />
       <Greeting name="Mike" favoriteNumber={42} />
       <Greeting name="Rod" favoriteNumber={43} />
       {/* <Greeting name="Brandon" favoriteNumber={3} /> */}
-    </>
+    </div>
   );
 }
 
