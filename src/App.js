ecbd156c8581e1597999c17ef2fc29f657c91319
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-09 18:29:13 -0600

    Showing my first React component...
    Maybe our second component...
    
    App is the "root" component...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting';
 
 const App = () => {
   return (
@@ -13,6 +14,10 @@ const App = () => {
       <h2>
         Hello Mike ! ! !
       </h2>
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
     </>
   );
 }
