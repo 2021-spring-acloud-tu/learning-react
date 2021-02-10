0b14ff32e0350f991dea48893bec0cb618bb108f
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-09 19:20:48 -0600

    Destructured props into individual variables...
    This is common, so that people can look at component definition and easily see the props it uses...

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,28 +1,32 @@
 import React from 'react';
 
-export const Greeting = (props) => {
+export const Greeting = ({
+    name
+    , favoriteNumber
+    , favoriteColors
+}) => {
     
-    console.log(props);
-
+    //console.log(props);
+    
     return (
         <>
             <h2>
-                Hello {props.name} ! ! !
+                Hello {name} ! ! !
             </h2>
             <h3>
-                {props.name}'s favorite number is {props.favoriteNumber}
+                {name}'s favorite number is {favoriteNumber}
             </h3>
             <h3>
-                {props.name}'s favorite number plus 10  is {props.favoriteNumber + 10}
+                {name}'s favorite number plus 10  is {favoriteNumber + 10}
             </h3>
             <h3>
-                {props.name}'s favorite colors are:
+                {name}'s favorite colors are:
             </h3>            
             <ul>
                 {
-                    props.favoriteColors 
+                    favoriteColors 
                         && [ 
-                            ...new Set(props.favoriteColors)
+                            ...new Set(favoriteColors)
                         ].map(x => (
                             <li 
                                 key={x}
@@ -31,7 +35,7 @@ export const Greeting = (props) => {
                             </li>)
                         )
                 }
-                {!props.favoriteColors && <li>None</li>}
+                {!favoriteColors && <li>None</li>}
             </ul>
         </>
     );
