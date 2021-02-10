c5e1f8a4cf739ce8a9a0bf9acd55d6c987907a43
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-09 18:53:43 -0600

    Passed an array prop...
    Also showed Boolean short circuit operator...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
       <h1>
         Hello React ! ! !
       </h1>
-      <Greeting name="Karly" favoriteNumber={9} />
+      <Greeting name="Karly" favoriteNumber={9} favoriteColors={["Red", "Purple", "Green"]} />
       <Greeting name="Mike" favoriteNumber={42} />
       <Greeting name="Rod" favoriteNumber={43} />
       {/* <Greeting name="Brandon" favoriteNumber={3} /> */}
